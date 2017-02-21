8dfd030c738e4151aa6bcc7f080f75b2841cc2e7
bindumuniraja <bindu4295@gmail.com>
2017-02-21 13:19:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,9 @@ console.log('Loading!');
 var element=document.getElementByid("main text");
 element.innerHTML="New value"
 
+//moveimage//
+var img=document.getElementBy("madi");
+ing.onclick=function() {
+    ing.style.marginLeft='100px';
+    
+};
