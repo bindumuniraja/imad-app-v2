facc331a89c53196549a092f9b3a727d8e803254
bindumuniraja <bindu4295@gmail.com>
2017-02-26 22:41:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,17 +2,23 @@
 var button=document.getElementById('counter');
 var counter=0;
 button.onclick=function(){
-    //Make a counter req to endpoint
-    
+    //create a request
+    var request= new XMLHttpRequest();
     // capture the req and store in variable
     
+    request.onreadystatechange = function(){
+       if(request.readyState === XMLHttpRequest().DONE) {
+           //take some action
+           if(request.status === 200){
+               var counter= request.responseText;
+               var span=document.getElementById('count');
+           }
+       }
+       //not done
+    };
+    //make a request
     
-    //render the variable in correct span
-    counter=counter+1;
-    var span=document.getElementById('count');
-    
-    span.innerHTML=counter.toString();
-    
-    
+    request.open('GET',"http://bindumuniraja.imad.hasura.io/counter",true);
+    request.send(null);
     
 };
