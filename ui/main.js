1c0b437f2bdb4f06d6431d0ef4ad56df3093701f
bindumuniraja <bindu4295@gmail.com>
2017-02-26 22:19:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,18 @@
-console.log('Loading!');
-var element=document.getElementById('main text');
-element.innerHTML = "New value";
-
-//moveimage//
-var img=document.getElementById('madi');
-img.onclick=function() {
-    img.style.marginLeft='100px';
+//Counter code
+var button=document.getElementById('counter');
+var counter=0;
+button.onclick=function(){
+    //Make a counter req to endpoint
+    
+    // capture the req and store in variable
+    
+    
+    //render the variable in correct span
+    counter=counter+1;
+    var span=document.getElementById('count');
+    
+    span.innerHTML=count.toString();
+    
+    
     
 };
